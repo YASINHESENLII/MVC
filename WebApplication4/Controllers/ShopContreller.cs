9f28be28151c7a3ede4217f0fb83a396e8b1ed5a
YASINHESENLII <yasinhesenli44@gmail.com>
2023-05-10 00:43:58 +0400

    MVC

---
+++ WebApplication4/Controllers/ShopContreller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class ShopContreller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Shop");
+        }
+        public IActionResult Details()
+        {
+            return View("singleproduct");
+        }
+
+    }
+}
---
+++ WebApplication4/Program.cs
@@ -0,0 +1,23 @@
+namespace WebApplication4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name:"Default",
+
+                pattern:"{controller}/{action}"
+                
+                );
+
+            app.Run();
+        }
+    }
+}
